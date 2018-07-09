39ca2694000601f1bfb9519b0e344ce397d53dbe
Raveen Bouy <raveenshenal@gmail.com>
2018-07-10 01:17:25 +0530

    JUST DO IT

--- Application1/Application1.py
+++ Application1/Application1.py
@@ -1,7 +1,11 @@
 from sklearn import tree
+from sklearn.svm.libsvm import predict
+from sklearn import svm
+from sklearn.linear_model import SGDClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors.nearest_centroid import NearestCentroid
 
-#Features are : Height, Weight, Shoe_Size
-
+#Sample set - Features are : Height, Weight, Shoe_Size
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37],
     [166, 65, 40], [190, 90, 47], [175, 64, 39], [177, 70, 40], 
     [159, 55, 37], [171, 75, 42], [181, 85, 43]]
@@ -9,9 +13,22 @@ X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37],
 Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
      'female', 'male', 'male']
 
-classifier = tree.DecisionTreeClassifier() # Using the decissiontree classifer to classify our data(female or male) 
 
-classifier = classifier.fit(X,Y) # Training the tree on the above given data set.
+#Classifiers
+classifier_tree = tree.DecisionTreeClassifier() # Using the decission tree classifer to classify our data(female or male) 
+classifier_svm  = svm.SVC() # Support vector machine classifier
+classifier_sgd  = SGDClassifier(loss="hinge", penalty="l2") # Stochastic Gradient descent classifier 
+classifier_mlc  = MLPClassifier(solver='lbfgs', alpha=1e-5, 
+                    hidden_layer_sizes=(5, 2), random_state=1) # Neural Network classifier
+classifier_nn   = NearestCentroid() # Using Nearest neighbor classifier
+
+
+#Training models
+classifier_tree = classifier_tree.fit(X,Y)  # Training the tree on the sample data set.
+classifier_svm  = classifier_svm.fit(X,Y)   # Training the SVM on the sample data set.
+classifier_sgd  = classifier_sgd.fit(X,Y)   # Training the SGD on the sample data set.
+classifier_mlc  = classifier_mlc.fit(X,Y)   # Training the sample set on multi-layer perceptron algorithm.
+classifier_nn   = classifier_nn.fit(X,Y)    # Training the NN classider on given data
 
 print("Welcome to gender classifier. Please fillout the following:")
 
@@ -19,6 +36,16 @@ UIHeight    = input("Height :")
 UIWeight    = input("Weight :")
 UIShoeSize  = input("Shoe_Size :")
 
-prediction  = classifier.predict([[UIHeight,UIWeight,UIShoeSize]])
+#Predicting the given value
+prediction_tree = classifier_tree.predict([[UIHeight, UIWeight, UIShoeSize]])
+prediction_svm  = classifier_svm.predict([[UIHeight, UIWeight, UIShoeSize]])
+prediction_sgd  = classifier_sgd.predict([[int(UIHeight), int(UIWeight), int(UIShoeSize)]])
+prediction_mlp  = classifier_mlc.predict([[int(UIHeight), int(UIWeight), int(UIShoeSize)]])
+prediction_nn   = classifier_nn.predict([[UIHeight,UIWeight,UIShoeSize]])
 
-print("Gender = ",prediction)
+#Printing the prediction
+print("Decision Tree : Gender = ",prediction_tree)
+print("Support Vector Machines : Gender = ", prediction_svm)
+print("Stochastic gradient descent : Gender = ", prediction_sgd)
+print("Multi-Layer Perceptron : Gender = ", prediction_mlp)
+print("Nearest Neighbor : Gender = ", prediction_nn)
