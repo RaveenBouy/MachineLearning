17d7b5040b4fe4c9ece0b12d679749b0abe1897c
Raveen Bouy <raveenshenal@gmail.com>
2018-07-15 17:20:23 +0530

    SEntiment analysis with csv

--- Application2/Application2.py
+++ Application2/Application2.py
@@ -4,9 +4,9 @@
 #For example, Subject, object and verb. Then the algorithm checks the number of tokenized words against the sentiment lexicon. 
 #In this case i'll be using tweepy, which is a library that could be used to access twitter API.Textblob is the library that does the analysis.
 
-
 import tweepy
 from textblob import TextBlob
+import csv
 
 APIKey              = "hbUbiPytE7sJBXkOPvcckzxcO"
 APISecretKey        = "B9I2b1mZ0pDaGfqBvpU3qXM1Szh8pl1Fyjmg4s7fSSR18RCR5c"
@@ -18,13 +18,18 @@ authenticate.set_access_token(accessToken, accessTokenSecret)
 
 API = tweepy.API(authenticate) #Authenticating with the Twitter API 
 
-tweets = API.search("Jeeebus")
+print("Welcome to Twitter Sentiment Analysis")
+sentimentSearch = input("Type the text you would like to search : ").encode("utf-8")
+print("\n")
+tweets = API.search(sentimentSearch)
+with open("TwitterSentimentAnalysis.csv", 'w', newline="") as TSA:
+     csvwriter = csv.writer(TSA)
+     csvwriter.writerow(["UserName","Tweet","Sentiment"])
 
-for tweet in tweets:
-    print(tweet.text)
-    analysis = TextBlob(tweet.text)
-    print(analysis.sentiment)
-    print("\n")
+     for tweet in tweets:
+        analysis = TextBlob(tweet.text)
+        csvwriter.writerow([tweet.user.screen_name, tweet.text, analysis.sentiment])
+
 
 
     
